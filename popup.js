1b0568e19a2960f166d878f6c991e8af15ad6805
Manu Cornet <m@ma.nu>
2022-04-12 15:30:38 +0000

    Add an event listener when clicking on checkboxes

--- popup.js
+++ popup.js
@@ -1,7 +1,21 @@
 let port;
 
+const onCheckboxChanged = () => {
+  const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+  console.log(checkboxes);
+};
+
 const initPopup = () => {
   const port = chrome.runtime.connect({ name: 'knockknock' });
+
+  const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+  console.log(checkboxes);
+  for (c of checkboxes) {
+    c.addEventListener('click', (e) => {
+      onCheckboxChanged();
+    });
+  }
+
   port.postMessage({
     event: 'poof',
     data: {  },
